e0118997510ae4b215a312fe16b6c02812659165
blcklamb <whenucan35@gmail.com>
2024-12-20 11:37:31 +0900

    ✨ 2-1, 2-2-1

---
+++ part2_basic/lib/01_material.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () {
+                print('tab');
+              },
+            ),
+            Icon(Icons.play_arrow)
+          ],
+          centerTitle: true,
+          title: Text('this is app bar'),
+        ),
+        body: TestWidget(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.bug_report),
+          onPressed: () => {print('bug report!')},
+        )),
+  ));
+}
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Text(
+          'Hello, Flutter',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 40,
+          ),
+        ),
+      ),
+    );
+  }
+}
